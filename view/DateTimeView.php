9952903c283c4f8a7ba357d5416a4d0249b88333
ah <ah224cy@student.lnu.se>
2018-09-24 18:05:00 +0200

    text right now

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,7 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = date("l") . ', the ' . date("jS") . date("F Y,") . ' The time is '  ;
+		$timeString = date("l") . ', the ' . date("jS") . " of " . date("F Y,") . ' The time is '  ;
 
 		return '<p>' . $timeString . '</p>';
 	}
