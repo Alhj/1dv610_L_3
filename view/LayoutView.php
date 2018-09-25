8754948562136cc59c20c69d8d8c54b8d7b0fd63
ah <ah224cy@student.lnu.se>
2018-09-25 20:13:48 +0200

    fix register view

--- view/LayoutView.php
+++ view/LayoutView.php
@@ -3,11 +3,16 @@
 
 class LayoutView {
 
-  $UserName = "RegisterView::UserName";
-  $passWord = "RegisterView::Password";
   
   public function render($isLoggedIn, LoginView $v, DateTimeView $dtv) {
-    if(isset($_GET["get"])){
+    if(isset($_GET["register"])){
+
+      $userName = "RegisterView::UserName";
+      $passWord = "RegisterView::Password";
+      $conPassWord = "RegisterView::PasswordRepeat";
+      $message = "RegisterView::Message";
+      $sumbit = "submit";
+
       echo '<!DOCTYPE html>
       <html>
         <head>
@@ -16,16 +21,31 @@ class LayoutView {
         </head>
         <body>
           <h1>Assignment 2</h1>
-          ' . $this->renderIsLoggedIn($isLoggedIn) . '
-          <a href="index.php">back</a>
+            <a href="index.php">back</a> '
+           . $this->renderIsLoggedIn($isLoggedIn) . '
           <div class="container">
-          <h2>Register new user</h2>
-           
-          </div>
+            <h2>Register new user</h2>
+              <form action ="">
+                <fieldset>
+                  <legend>Register a new user - Write username and password</legend>
+                  <p id = "'. $message .'"> </p>
+
+                  <label id = " '. $userName . '">Username:</label>
+                  <input id = " '. $userName . '"  type = "text">
+                  <br>
+                  <label id = " '. $passWord . '">Password:</label>
+                  <input id = " '. $passWord . '" type="password">
+                  <br>
+                  <label id = " '. $conPassWord . '">Repeat password:</label>
+                  <input id = " '. $conPassWord . '" type="password">
+                  <br>
+                  <input id= " '. $sumbit. ' "  type="submit" value="Register">
+                </fieldset>
+             </form>
+             ' . $dtv->show() . '
+            </div>
           </body>
-       </html>'
-          
-          ;
+       </html>';
     } else {
     echo '<!DOCTYPE html>
       <html>
@@ -35,8 +55,8 @@ class LayoutView {
         </head>
         <body>
           <h1>Assignment 2</h1>
+          ' . $this->link($isLoggedIn) . '
           ' . $this->renderIsLoggedIn($isLoggedIn) . '
-          <a href="index.php?get">Register a new user</a>
           <div class="container">
               ' . $v->response() . '
               
@@ -48,12 +68,21 @@ class LayoutView {
     }
   }
   
+
   private function renderIsLoggedIn($isLoggedIn) {
     if ($isLoggedIn) {
       return '<h2>Logged in</h2>';
     }
     else {
       return '<h2>Not logged in</h2>';
+    }
+  }
+
+  private function link ($isLoggedIn) {
+    if($isLoggedIn) {
+      return '';
+    } else {
+      return '<a href="index.php?register">Register a new user</a>';
     }
   }
 }
