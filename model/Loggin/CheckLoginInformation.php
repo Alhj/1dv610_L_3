202c851a23cb59eb4fa300387845c39307370b04
ah <ah224cy@student.lnu.se>
2018-10-19 09:48:51 +0200

    env file in env.php.defult have the code but not the info

--- model/Loggin/CheckLoginInformation.php
+++ model/Loggin/CheckLoginInformation.php
@@ -7,8 +7,6 @@ class logginCheck
     public function checkLogginInformation($userName, $password)
     {
 
-        $hash = '$2y$10$Z/DCVZpx9581g5bjn3JeuuaHBuud0Dn4fkPVdGdObOkGNqmNf6z1m';
-
         if ($userName === getenv("userName")) {
 
             if (password_verify($password, getenv("passWord"))) {
